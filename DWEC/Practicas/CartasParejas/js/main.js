927cff9b10ab7952634f2c56d8fdacc6517fb03f
jlrl <goldteamspain@gmail.com>
2023-12-15 23:10:25 +0100

    primer Commit

---
+++ DWEC/Practicas/CartasParejas/js/main.js
@@ -0,0 +1,7 @@
+let cartasArray = [];
+let cartasEscogidas = [];
+let carta = "";
+const palos = ["C", "D", "H", "S"];
+const figuras = ["2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+
+inicioJuego();
